b3c45b93afee9894081faa674d3d0c6ec424c864
Damian Arechar <damian.lon.are@gmail.com>
2020-09-05 17:48:43 -0500

    Removed logo image & a tag from the Header component

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ import './App.css';
 function Header() {
   return (
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
       <p>
-        Edit <code>src/App.js</code> and save to reload.
+        Welcome to iSeries.com!
       </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
     </header>
   )
 }
