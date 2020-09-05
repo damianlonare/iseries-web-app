abef3cc9230d5c180f367d7cbc8723b27c473828
Damian Arechar <damian.lon.are@gmail.com>
2020-09-05 18:56:57 -0500

    Added some header component tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
+import Header from './layouts/Header'
 
-test('renders title text', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Welcome to iSeries.com!/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  test('renders App component', () => {
+    render(<App />)
+  })
+})
+
+describe('layouts', () => {
+  test('renders Header component', () => {
+    const { getByTestId, getByText } = render(<Header />)
+
+    expect(getByTestId("header")).toHaveClass("app-header")
+    expect(getByText('iSeries')).toBeInTheDocument()
+  })
+})
