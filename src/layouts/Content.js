484422ba5bc90d9ff9a51cf5d51be574a7c236e4
Damian Arechar <damian.lon.are@gmail.com>
2020-09-05 18:57:16 -0500

    Create Content.js

---
+++ src/layouts/Content.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Content() {
+    return (
+      <header className="App-content">
+        <p>
+          Welcome to iSeries.com!
+        </p>
+      </header>
+    )
+}
+
+export default Content
