36579aeb37653e8dc94cf5a5c1789ab47227e78c
Damian Arechar <damian.lon.are@gmail.com>
2020-09-06 03:59:56 -0500

    Eslint init

--- src/layouts/Content.js
+++ src/layouts/Content.js
@@ -1,45 +1,38 @@
-import React from 'react';
+import React from 'react'
 import './Content.css'
 import Table from '../components/Table'
 
 function Content() {
-    return (
-      <section role="main" className="app-content">
-        <p>Ordenar:</p>
-
-        <span>Alfabéticamente:</span>
-        {/* TODO: QUITAR, SE AGREGÓ SOLO PARA AYUDAR A ENTENDER MEJOR 
+  return (
+    <section role="main" className="app-content">
+      <p>Ordenar:</p>
+      <span>Alfabéticamente:</span>
+      {/* TODO: QUITAR, SE AGREGÓ SOLO PARA AYUDAR A ENTENDER MEJOR 
         ANTES DE LA IMPLEMENTACIÓN DE DISEÑO FINAL */}
-        &nbsp;
-
-        <button>A-Z</button>
-        <button>Z-A</button>
-
-        {/* TODO: QUITAR, SE AGREGÓ SOLO PARA AYUDAR A ENTENDER MEJOR 
+      &nbsp;
+      <button>A-Z</button>
+      <button>Z-A</button>
+      {/* TODO: QUITAR, SE AGREGÓ SOLO PARA AYUDAR A ENTENDER MEJOR 
         ANTES DE LA IMPLEMENTACIÓN DE DISEÑO FINAL */}
-        &nbsp;
-
-        <span>o por puntuación:</span>
-
-        {/* TODO: QUITAR, SE AGREGÓ SOLO PARA AYUDAR A ENTENDER MEJOR 
+      &nbsp;
+      <span>o por puntuación:</span>
+      {/* TODO: QUITAR, SE AGREGÓ SOLO PARA AYUDAR A ENTENDER MEJOR 
         ANTES DE LA IMPLEMENTACIÓN DE DISEÑO FINAL */}
-        &nbsp;
-
-        <button>5 a 0</button>
-        <button>0 a 5</button>
-
-        {/* TODO: QUITAR, SE AGREGÓ SOLO PARA AYUDAR A ENTENDER MEJOR 
-        ANTES DE LA IMPLEMENTACIÓN DE DISEÑO FINAL */}        
-        <br /><br />
-
-        <div className="btn-menu-group">
-          <button>Populares</button>
-          <button>Mejor valoradas</button>
-          <button>Mas vistas</button>
-        </div>
-        <Table />
-      </section>
-    )
+      &nbsp;
+      <button>5 a 0</button>
+      <button>0 a 5</button>
+      {/* TODO: QUITAR, SE AGREGÓ SOLO PARA AYUDAR A ENTENDER MEJOR 
+        ANTES DE LA IMPLEMENTACIÓN DE DISEÑO FINAL */}
+      <br />
+      <br />
+      <div className="btn-menu-group">
+        <button>Populares</button>
+        <button>Mejor valoradas</button>
+        <button>Mas vistas</button>
+      </div>
+      <Table />
+    </section>
+  )
 }
 
 export default Content
