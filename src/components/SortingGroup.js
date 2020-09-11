16ce258060e3e727130f6ebed7e1b4d5afa5ca31
Damian Arechar <damian.lon.are@gmail.com>
2020-09-11 00:33:23 -0500

    Removed SortingGroup's comments

--- src/components/SortingGroup.js
+++ src/components/SortingGroup.js
@@ -5,26 +5,12 @@ function SortingGroup({ handleOnClickOrderBy, orderBy }) {
   return (
     <div>
       <p>Ordenar:</p>
-      <span>Alfabéticamente:</span>
-      {/* TODO: QUITAR, SE AGREGÓ SOLO PARA AYUDAR A ENTENDER MEJOR 
-        ANTES DE LA IMPLEMENTACIÓN DE DISEÑO FINAL */}
-      &nbsp;
       <button onClick={() => handleOnClickOrderBy('A-Z')}>A-Z</button>
       <button onClick={() => handleOnClickOrderBy('Z-A')}>Z-A</button>
-      {/* TODO: QUITAR, SE AGREGÓ SOLO PARA AYUDAR A ENTENDER MEJOR 
-        ANTES DE LA IMPLEMENTACIÓN DE DISEÑO FINAL */}
-      &nbsp;
-      <span>o por puntuación:</span>
-      {/* TODO: QUITAR, SE AGREGÓ SOLO PARA AYUDAR A ENTENDER MEJOR 
-        ANTES DE LA IMPLEMENTACIÓN DE DISEÑO FINAL */}
-      &nbsp;
       <button onClick={() => handleOnClickOrderBy('100-0')}>100 a 0</button>
       <button onClick={() => handleOnClickOrderBy('0-100')}>0 a 100</button>
-      {/* TODO: QUITAR, SE AGREGÓ SOLO PARA AYUDAR A ENTENDER MEJOR 
-        ANTES DE LA IMPLEMENTACIÓN DE DISEÑO FINAL */}
       <br />
       <br />
-      <p>Seleccionado: {orderBy}</p>
     </div>
   )
 }
