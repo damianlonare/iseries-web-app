f71bf673560f72a25813bf2f0d6a2029e9a66664
Damian Arechar <damian.lon.are@gmail.com>
2020-09-11 11:31:58 -0500

    Update Header.js

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -3,13 +3,14 @@ import './Header.css'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Toolbar'
+import { makeStyles } from '@material-ui/core/styles'
 
 function Header() {
   return (
     <AppBar position="static">
-      <Typography variant="h6" className={classes.title}>
-        iSeries
-      </Typography>
+      <Toolbar>
+        <Typography variant="h6">iSeries</Typography>
+      </Toolbar>
     </AppBar>
   )
 }
