07dcaa020a82f9ff1b92477ddd903e2bb9a4d96b
Damian Arechar <damian.lon.are@gmail.com>
2020-09-11 10:39:06 -0500

    WIP

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './Header.css'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Toolbar'
 
 function Header() {
   return (
-    <header role="banner">
-      <h1>iSeries</h1>
-    </header>
+    <AppBar position="static">
+      <Typography variant="h6" className={classes.title}>
+        iSeries
+      </Typography>
+    </AppBar>
   )
 }
 
