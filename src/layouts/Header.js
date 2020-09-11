1df85d82b969356dcbf4877bea1eee8129acd71e
Damian Arechar <damian.lon.are@gmail.com>
2020-09-11 12:37:28 -0500

    Update Header's UI

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -5,11 +5,25 @@ import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Toolbar'
 import { makeStyles } from '@material-ui/core/styles'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}))
+
 function Header() {
+  const classes = useStyles()
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6">iSeries</Typography>
+        <Typography variant="regular">iSeries</Typography>
       </Toolbar>
     </AppBar>
   )
